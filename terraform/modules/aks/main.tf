ce1a32cc89619544737e5c2b71e427893908d2a8
eugene <eugene@terraform.myguest.virtualbox.org>
2024-07-19 23:51:48 +0800

    Edited kubernetes deployment files and disabled kubernetes local account

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -19,6 +19,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
    ingress_application_gateway {
      subnet_id = var.agw_subnet
  }
+   local_account_disabled = true
    azure_active_directory_role_based_access_control {
      managed = true
      azure_rbac_enabled = false
